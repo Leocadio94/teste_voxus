c2a7d6c4b72757001cdeddb607a1e4a0784cc1ba
Gabriel Leocádio Almeida da Silva <gabriel.silva73@fatec.sp.gov.br>
2016-06-02 02:10:38 -0300

    Versão inicial

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge; charset=utf-8"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+        <meta name="description" content="Site de Noticias"></meta>
+        <meta name="author" content="Gabriel Leocadio"></meta>
+        
+        <title>Noticias - Voxus.tv</title>
+        
+        <link rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"></link>
+        <link rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"></link>
+        <link rel="stylesheet" 
+        href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"></link>  
+            
+        <link rel="stylesheet" href="css/styles.css"></link>
+    </head>
+    <body>  
+        <div class="container">
+              <?php require 'templates/navegacao.php' ?>
+              <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header text-center">Cadastro de Notícias</h1>
+                    
+                    <form>    
+                        <div class="form-group">
+                        </div>
+                    </form>
+                    <hr/>
+                    <div class="footer text-center center-block">
+                        <span>Desenvolvido por Gabriel Leocádio - <a href="https://br.linkedin.com/in/gabrielleocadio" target="_blank"><i class="fa fa-linkedin"></i>
+                        Linkedin</a> / <a href="https://github.com/Leocadio94" target="_blank"><i class="fa fa-github"></i>
+                         GitHub</a></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </body>
+</html>
---
+++ templates/navegacao.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php"><i class="fa fa-home fa-fw"> </i> Notícias</a>
+    </div>
+    <div id="navbar" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="index.php">Cadastro de Notícias</a></li>
+        <li><a href="ver_noticias.php">Leitura de Notícias</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
